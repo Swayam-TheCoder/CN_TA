907b075fa78d548990be5f9b1638f3661d2ce1a5
Swayam <swayamcode1@gmail.com>
2025-10-27 23:29:17 +0530

    connection improved

--- Backend/app.py
+++ Backend/app.py
@@ -2,9 +2,20 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
 from datetime import datetime
+import os
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT", 3306)
+    )
 
 # MySQL Database Configuration
 
